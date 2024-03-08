3ca46fe92a702ce33be0951ef49350c0bc16f419
Dylan Laurianti <dlaurianti@uiowa.edu>
2024-03-07 18:06:01 -0600

    init: install firebase & deps

---
+++ app.js
@@ -0,0 +1,5 @@
+var firebase = require ('firebase/app');
+var nodeimu = require ('@trbll/nodeimu');
+var IMU = new nodeimu.IMU( );
+var sense = require ('@trbll/sense-hat-led');
+const { getDatabase, ref, onValue, set, update, get } = require ('firebase/database');
