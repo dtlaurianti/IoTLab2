5718b1458c55097e49b9be4dd8ba68913d3d710e
Dylan Laurianti <dlaurianti@uiowa.edu>
2024-03-07 18:40:58 -0600

    feat: config and update_light watcher

--- app.js
+++ app.js
@@ -18,8 +18,23 @@ const firebaseConfig = {
 
   messagingSenderId: "618734441215",
 
-  appId: "1:618734441215:web:8b8600096d8f920b7a72ca"
+  appId: "1:618734441215:web:8b8600096d8f920b7a72ca",
 
+  databaseURL: "https://lab2-1a0d4-default-rtdb.firebaseio.com/",
+
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const database = getDatabase();
+
+const updateLightRef = ref(database, "update_light");
+onValue(updateLightRef, (snapshot) => {
+  const data = snapshot.val();
+  updateLightInfo();
+});
+
+function updateLightInfo() {
 };
 
 firebase.initializeApp('firebaseConfig');
