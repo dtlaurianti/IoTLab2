84eaeadbbf4e7aeb038d3b14e0f981e4c7bb2b36
Dylan Laurianti <dlaurianti@uiowa.edu>
2024-03-07 18:10:30 -0600

    chore: firebase config

--- app.js
+++ app.js
@@ -3,3 +3,22 @@ var nodeimu = require ('@trbll/nodeimu');
 var IMU = new nodeimu.IMU( );
 var sense = require ('@trbll/sense-hat-led');
 const { getDatabase, ref, onValue, set, update, get } = require ('firebase/database');
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDAeyJDTBRBZVrfA0F71dJAlS2Ibl52ZHU",
+
+  authDomain: "lab2-1a0d4.firebaseapp.com",
+
+  projectId: "lab2-1a0d4",
+
+  storageBucket: "lab2-1a0d4.appspot.com",
+
+  messagingSenderId: "618734441215",
+
+  appId: "1:618734441215:web:8b8600096d8f920b7a72ca"
+
+};
+
